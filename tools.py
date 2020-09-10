d5602ef8e7e57a071be9c602470e5aa7a451c7f0
Bendik Nordeng <bendiknor@gmail.com>
2020-09-10 19:47:33 +0200

    Fill missing values with mean

--- tools.py
+++ tools.py
@@ -18,6 +18,7 @@ def load_data():
     df.calendar_last_scraped = pd.to_datetime(df.calendar_last_scraped, format='%Y-%m-%d')
     df.first_review = pd.to_datetime(df.first_review, format='%Y-%m-%d')
     df.last_review = pd.to_datetime(df.last_review, format='%Y-%m-%d')
+    df.dropna(subset=['host_since']) # One value
 
     # Convert 't' to 1 and 'f' to 0
     df.host_is_superhost = df.host_is_superhost.apply(lambda x: 1 if x=='t' else 0)
@@ -44,5 +45,22 @@ def load_data():
     # Add binary column for missing acceptance rate and set missing values to 0
     df.acceptance_rate_missing = df.host_acceptance_rate.notnull().astype(int)
     df.host_acceptance_rate.fillna(0, inplace=True)
+
+    # Set missing values for reviews to mean
+    df.review_scores_rating.fillna(df.review_scores_rating.mean(), inplace=True)
+    df.review_scores_accuracy.fillna(df.review_scores_accuracy.mean(), inplace=True)
+    df.review_scores_cleanliness.fillna(df.review_scores_cleanliness.mean(), inplace=True)
+    df.review_scores_checkin.fillna(df.review_scores_checkin.mean(), inplace=True)
+    df.review_scores_communication.fillna(df.review_scores_communication.mean(), inplace=True)
+    df.review_scores_location.fillna(df.review_scores_location.mean(), inplace=True)
+    df.review_scores_value.fillna(df.review_scores_value.mean(), inplace=True)
+
+    # Set missing values for reviews per month to 0 (never gotten reviews)
+    df.reviews_per_month.fillna(0, inplace=True)
+
+    # Fill missing roomspecifications with mean
+    df.bathrooms.fillna(df.bathrooms.mean(), inplace=True)
+    df.bedrooms.fillna(df.bedrooms.mean(), inplace=True)
+    df.beds.fillna(df.beds.mean(), inplace=True)
 
     return df
