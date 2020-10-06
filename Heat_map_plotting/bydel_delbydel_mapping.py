2de39d7e22bc322d0d1141f15a0ef0241e872c2b
Peder Lund <peder.m.lund@gmail.com>
2020-10-06 13:09:32 +0200

    Done for the day
    Done for the day

--- Heat_map_plotting/bydel_delbydel_mapping.py
+++ Heat_map_plotting/bydel_delbydel_mapping.py
@@ -11,12 +11,14 @@ WRITE_EXCEL = True
 
 # Read in the the separate data sources to pandas
 df = pd.read_excel(r'../data/heat_map_bydeler_delbydeler_grunnkretser.xlsx', sheet_name='Table 1')
+df2 = pd.read_csv(r'../data/neighbourhoods.csv')
 gdf = geopandas.GeoDataFrame.from_file("../data/heat_map_grunnkretser_oslo.geojson")
 
 # Convert object to int64
 gdf['grunnkretsnummer'] = gdf['grunnkretsnummer'].astype(int)
 
 # Merge the data based on grunnkretsnummer
+df = pd.merge(df, df2, left_on='Bydel_Navn', right_on='neighbourhood')
 all_data = pd.merge(gdf, df, left_on='grunnkretsnummer', right_on='grunnkretsnummer')
 
 # Create set of dataframes
@@ -33,7 +35,7 @@ all_data = pd.concat(gdfs)
 all_data.crs = "EPSG:25832"
 
 # Reduce the number of columns
-bydel = all_data[['geometry', 'Bydel_ID', 'Bydel_Navn']].copy()
+bydel = all_data[['geometry', 'Bydel_ID', 'Bydel_Navn', "neighbourhood_group"]].copy()
 
 # Calculate the square km per bydel
 bydel['Bydel_area'] = bydel['geometry'].to_crs({'init': 'epsg:4326'}).map(lambda p: p.area / 10 ** 6)
--- Heat_map_plotting/generate_plots.py
+++ Heat_map_plotting/generate_plots.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import geopandas
 import folium as folium
+from shapely.geometry import Point
+from folium.plugins import FloatImage
 
 # READ ME
 # This file generates and saved different heat maps.
@@ -10,28 +12,30 @@ import folium as folium
 BYDEL = True
 
 # Data sources
+LISTINGS = '../Data/listings_detailed.csv'
 if BYDEL:
-    HEAT_MAP_DATA_FILE_NAME = '../Data/heat_map_bydel_plotting_values.xlsx'
+    HEAT_MAP_DATA_FILE_NAME = '../Data/heat_map_bydel_plotting_values_test.xlsx'
     AREA_ID_COLUMN_NAME = 'Bydel_ID'
     GEO_DATA_FILE_NAME = '../Data/heat_map_bydel_boundaries.geojson'
-    HOOVER_FIELD = ['Bydel_Navn']
+    HOOVER_FIELD = ['Bydel_Navn', 'mean_price']
+    ALIASES = ['Neighbourhood: ', 'Mean price: ']
 else:
     HEAT_MAP_DATA_FILE_NAME = '../Data/heat_map_delby_plotting_values.xlsx'
     AREA_ID_COLUMN_NAME = 'Delbydel_ID'
     GEO_DATA_FILE_NAME = '../Data/heat_map_delby_boundaries.geojson'
-    HOOVER_FIELD = ['Delbydel_Navn']
+    HOOVER_FIELD = ['Delbydel_Navn', 'mean_price']
 
 # Values to be plotted
-HEAT_MAP_DATA_SHEET_NAMES = ['Input_inntekt']                   # ['Input_inntekt', 'Input_folketall', 'Input_befolkningstetthet_20-40']
-HEAT_MAP_DATA_COLUMN_NAMES = ['Inntekt 2017']                   # ['Inntekt 2017', 'Folketall 2020', 'Befolkningstetthet_20-40']
-HEAT_MAP_TITLES = ['Income Oslo 2017']                          # ['Income Oslo 2017', 'Population Oslo 2020', 'Population density Oslo']
-HEAT_MAP_OUTPUT_FILE_NAMES = ['heat_map_income_5.html']         # ['Income Oslo 2017', 'Population.html', 'Population_density_20-40.html']
+HEAT_MAP_DATA_SHEET_NAMES = ['Sheet1']                   # ['Input_inntekt', 'Input_folketall', 'Input_befolkningstetthet_20-40']
+HEAT_MAP_DATA_COLUMN_NAMES = ['mean_price']                   # ['Inntekt 2017', 'Folketall 2020', 'Befolkningstetthet_20-40']
+HEAT_MAP_TITLES = ['Mean price AirBnB']                          # ['Income Oslo 2017', 'Population Oslo 2020', 'Population density Oslo']
+HEAT_MAP_OUTPUT_FILE_NAMES = ['mean_price_AirBnB.html']         # ['Income Oslo 2017', 'Population.html', 'Population_density_20-40.html']
 
 # Map specifications
 CENTER_OF_MAP = [59.91, 10.73]
-PLOTTING_COLOR = 'YlOrRd'                                         # YlGnBu, BuPu, Spectral, BrBg, Blues, Greys
+PLOTTING_COLOR = ['YlOrRd']                                     # YlGnBu, BuPu, Spectral, BrBg, Blues, Greys
 PLOTTING_FILL_OPACITY = 0.3
-ZOOM = 11
+ZOOM = 12
 TILES = 'cartodbpositron'
 
 
@@ -51,12 +55,16 @@ def main():
 
     # Plot heat map data
     for i in range(len(HEAT_MAP_DATA_SHEET_NAMES)):
-        heatMapPlot = plot(delby, ['geoid', HEAT_MAP_DATA_COLUMN_NAMES[i]], HEAT_MAP_TITLES[i], PLOTTING_COLOR,
+        heatMapPlot = plot(delby, ['geoid', HEAT_MAP_DATA_COLUMN_NAMES[i]], HEAT_MAP_TITLES[i], PLOTTING_COLOR[i],
                            PLOTTING_FILL_OPACITY)
         heatMapPlot.save('../Plots/' + HEAT_MAP_OUTPUT_FILE_NAMES[i])
 
+    # Plot listings
+    listings_map = plot_listings()
+    listings_map.save('../Plots/cluster_listing.html')
 
-def plot(data, plot_list, legend_name, colour='YlOrRd', fill_opacity=0.2):
+
+def plot(data, plot_list, legend_name, colour, fill_opacity=0.2):
     # Create a Map instance
     m = folium.Map(location=CENTER_OF_MAP, tiles=TILES, zoom_start=ZOOM, control_scale=True)
 
@@ -78,7 +86,49 @@ def plot(data, plot_list, legend_name, colour='YlOrRd', fill_opacity=0.2):
     folium.LayerControl().add_to(m)
 
     # Display Region Label
-    # m.geojson.add_child(folium.features.GeoJsonTooltip(HOOVER_FIELD, labels=False))
+    m.geojson.add_child(folium.features.GeoJsonTooltip(fields=HOOVER_FIELD, aliases=ALIASES, labels=True))
+    return m
+
+
+def plot_listings():
+    df = pd.read_csv(LISTINGS)
+    locs_geometry = [Point(xy) for xy in zip(df.longitude, df.latitude)]
+    crs = {'init': 'epsg:4326'}
+    # Coordinate Reference Systems, "epsg:4326" is a common projection of WGS84 Latitude/Longitude
+    locs_gdf = geopandas.GeoDataFrame(df, crs=crs, geometry=locs_geometry)
+    m = folium.Map(location=[59.91, 10.73], zoom_start=10, tiles='cartodbpositron')
+
+    feature_ea = folium.FeatureGroup(name='Entire home/apt')
+    feature_pr = folium.FeatureGroup(name='Private room')
+
+    for i, v in locs_gdf.iterrows():
+        hoover_text = """
+        Price: <b>%s</b><br>
+        Minimum nights: <b>%s</b><br>
+        URL: <b>%s</b><br>
+        """ % (v['price'], v['minimum_nights'], v['listing_url'])
+        popup_text ="Price: %s \nMinimum nights: %s \nURL: %s" % (v['price'], v['minimum_nights'], v['listing_url'])
+        popup = folium.Popup(popup_text, parse_html=True)
+
+        if v['room_type'] == 'Entire home/apt':
+            folium.CircleMarker(location=[v['latitude'], v['longitude']],
+                                radius=5,
+                                tooltip=hoover_text,
+                                popup=popup,
+                                color='#FFBA00',
+                                fill_color='#FFBA00',
+                                fill=True).add_to(feature_ea)
+        elif v['room_type'] == 'Private room':
+            folium.CircleMarker(location=[v['latitude'], v['longitude']],
+                                radius=5,
+                                tooltip=hoover_text,
+                                popup=popup,
+                                color='#087FBF',
+                                fill_color='#087FBF',
+                                fill=True).add_to(feature_pr)
+    feature_ea.add_to(m)
+    feature_pr.add_to(m)
+    folium.LayerControl(collapsed=False).add_to(m)
     return m
 
 main()
